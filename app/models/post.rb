a92134f0d038e54f28979105f253416094a8c08c
Micheal & Drew <student@epicodus.com>
2015-07-01 11:10:54 -0700

    refactor time formatting into method on post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,8 @@
 class Post  < ActiveRecord::Base
   validates :user, :presence => true
   validates :title, :presence => true
+
+  def formatted_time
+    return self.created_at.in_time_zone("Pacific Time (US & Canada)").strftime("%A, %B %d at %I:%M %p  %Z")
+  end
 end

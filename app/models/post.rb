ce48028ad42a3687a4f7b5eb46345b156ef2ac94
Micheal & Drew <student@epicodus.com>
2015-07-01 12:12:56 -0700

    add average rating from user reviews

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,4 +5,13 @@ class Post  < ActiveRecord::Base
   def formatted_time
     return self.created_at.in_time_zone("Pacific Time (US & Canada)").strftime("%A, %B %d at %I:%M %p  %Z")
   end
+
+  def average_review_score
+    result = self.reviews.average(:rating)
+    if result.is_a? Numeric
+      return result.round
+    else
+      return 1
+    end
+  end
 end
